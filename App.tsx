dcc58a5bc9fe3594868a5c35bc3a9ed3360d50b6
Jhonatan Kennedy <jhonatankennedy@live.com>
2023-12-23 20:12:27 -0300

    initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
+import { type ReactElement } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default function App() {
+export default function App(): ReactElement<any, any> {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Start High</Text>
       <StatusBar style="auto" />
     </View>
   );
